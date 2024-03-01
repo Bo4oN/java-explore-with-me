931c360c9aba2f58756e27f4d50bb6f91a9a00a5
Михаил Васильев <obliviom@Air-Mihail.lan>
2024-03-01 16:29:26 +0300

    сервис статистики

--- statistics/client/src/main/java/practicum/client/StatsClient.java
+++ statistics/client/src/main/java/practicum/client/StatsClient.java
@@ -2,12 +2,11 @@ package practicum.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import stats.StatsDto;
 
 import java.time.LocalDateTime;
