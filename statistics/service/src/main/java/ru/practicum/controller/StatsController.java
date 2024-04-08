1cf57fae9b17ed0c0a10514cb9f7204ad8254265
Михаил Васильев <obliviom@Air-Mihail.lan>
2024-04-08 05:33:15 +0300

    правки в с сервис статистики

--- statistics/service/src/main/java/ru/practicum/controller/StatsController.java
+++ statistics/service/src/main/java/ru/practicum/controller/StatsController.java
@@ -11,6 +11,7 @@ import ru.practicum.service.StatsServiceImpl;
 import stats.StatsDto;
 import stats.StatsDtoOut;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -22,6 +23,7 @@ public class StatsController {
     private final StatsServiceImpl service;
 
     @PostMapping("/hit")
+    @ResponseStatus(HttpStatus.CREATED)
     public void saveInformation(@RequestBody StatsDto statsDto) {
         log.info("Save data - {}, to statistics", statsDto);
         service.saveStats(statsDto);
@@ -30,9 +32,11 @@ public class StatsController {
     @GetMapping("/stats")
     public ResponseEntity<List<StatsDtoOut>> getStatistics(@RequestParam(value = "start")
                                                            @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+                                                           @NotNull
                                                            LocalDateTime start,
                                                            @RequestParam(value = "end")
                                                            @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+                                                           @NotNull
                                                            LocalDateTime end,
                                                            @RequestParam(required = false, value = "uris") @Nullable List<String> uris,
                                                            @RequestParam(value = "unique", defaultValue = "false") boolean isUnique) {
